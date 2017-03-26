89638a48a97b4d2e4a88bb4811db3258a6b6b7ee
VIJAYA <VIJAYA@192.168.0.4>
2017-03-26 12:18:01 +0530

    @sangeetha modified demo1 file

--- AutomationProject/src/pack1/Demo1.java
+++ AutomationProject/src/pack1/Demo1.java
@@ -5,5 +5,6 @@ public class Demo1 {
 	public void display(){
 		System.out.println("this is vijaya kumari..");
 		System.out.println("sangeetha added some code...");
+		System.out.println("Hello");
 	}
 }
